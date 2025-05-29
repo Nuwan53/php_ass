0ac86da20d1ee08a8128f69c2e1a33a4edd170c6
Nuwan <nuwanrathnayaka53@gmail.com>
2025-05-29 23:06:43 +0530

    arr

--- ass3/q1.php
+++ ass3/q1.php
@@ -5,5 +5,14 @@
             echo $i . $j;
             echo "</br>";
         }
+     $even_numbers = array(2,4,6,8);
+     foreach ($even_numbers as $even) {
+         echo $even;
+         echo "</br>";
+     }
+        $odd_numbers = array(1,3,5,7,9);
+        foreach ($odd_numbers as $odd) {
+            echo $odd;
+            echo "</br>"; 
     }
 ?>
